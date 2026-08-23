6b55a30b1f8922633f035a0c8cf2c623eab77c79
ujjaldas-11 <dasu82058@gmail.com>
2026-08-24 01:09:44 +0530

    chore: initialize Typescript Express backend

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.json({
+    message: "LifeOS API is running 🚀"
+  });
+});
+
+export default app;
